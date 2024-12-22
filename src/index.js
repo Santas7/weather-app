cf96b6ebabffdc814f1253f7af102d9373fce048
Santas7 <andreypanyshkin03@gmail.com>
2024-12-22 02:18:33 -0800

    feat(index): add imports for assets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './styles/styles.sass';
 
+import rainSound from './assets/sounds/rain.mp3';
+import summerSound from './assets/sounds/summer.mp3';
+import winterSound from './assets/sounds/winter.mp3';
+
+import rainIcon from './assets/icons/cloud-rain.svg';
+import summerIcon from './assets/icons/sun.svg';
+import winterIcon from './assets/icons/cloud-snow.svg';
+import pauseIcon from './assets/icons/pause.svg';
+
+import rainBg from './assets/images/rainy-bg.jpg';
+import summerBg from './assets/images/summer-bg.jpg';
+import winterBg from './assets/images/winter-bg.jpg';
+
+
 const app = document.getElementById('app');
