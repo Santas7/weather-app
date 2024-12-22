6150347762d0d2eb88ca7212fbfa326bcbc9da4f
Santas7 <andreypanyshkin03@gmail.com>
2024-12-22 02:43:10 -0800

    feat: add CopyPlugin & new rules

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     entry: './index.js',
     output: {
-        filename: 'bundle.[contenthash].js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
@@ -13,14 +16,27 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public/index.html'),
         }),
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+              { from: path.resolve(__dirname, 'public/favicon.ico'), to: path.resolve('dist')},
+            ],
+        }),
     ],
     module: {
         rules: [
             {
                 test: /\.s[ac]ss$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|mp3|wav)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ],
     }
 };
